74be9ebe79764fe77fd14489a667e6dc144b539d
yunji0387 <yunji0387@gmail.com>
2023-06-09 18:13:57 -0500

    update webpage: weather api does not worked yet

--- app.js
+++ app.js
@@ -5,18 +5,47 @@ const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const _ = require("lodash");
 const mongoose = require("mongoose");
+const https = require("https");
+const date = require(__dirname + "/logic/date.js");
 
 const app = express();
 
-app.set("view engine", "ejs");
+let currentTime = date.getFullDate(); 
 
+app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-//mongoose.connect("", {useNewURLParser: true});
+mongoose.connect("mongodb://localhost:27017/dailyWebDB", { useNewUrlParser: true });
+
+const weatherInfoSchema = new mongoose.Schema({
+    name: String,
+    temp: Number,
+    description: String
+});
+
+const weatherKeySchema = new mongoose.Schema({
+    key: String
+});
+
+const WeatherKey = mongoose.model("WeatherKey", weatherKeySchema, 'weatherkeys');
 
 app.get("/", function(req, res){
-    res.render("home");
+    let currentKey = "";
+    WeatherKey.find({key: 1})
+    .then(function(foundKey){
+        if(foundKey){
+            currentKey = foundKey[0].key.toString();
+            console.log(foundKey);
+        }else{
+            currentKey = "No!!!";
+        }
+        // Render the EJS template inside the promise's then block
+        res.render("home", { currentTime: currentTime, currKey: currentKey });
+    })
+    .catch(function(err){
+        console.log(err);
+    });
 });
 
 app.post("/", function(req, res){
---
+++ logic/date.js
@@ -0,0 +1,71 @@
+//jshint esversion:6
+
+function getTime(){ //example return output = 11:19pm
+    const currentDate = new Date();
+    const hours = currentDate.getHours();
+    const minutes = currentDate.getMinutes();
+    const period = hours >= 12 ? 'pm' : 'am';
+    const formattedHours = hours % 12 || 12;
+    const formattedMinutes = minutes.toString().padStart(2, '0');
+    return `${formattedHours}:${formattedMinutes}${period}`;
+}
+
+function getDay(){ //example return output = 10
+    const currentDate = new Date();
+    const day = currentDate.getDate();
+    return day;
+}
+
+function getMonth(){ //example return output = December
+    const currentDate = new Date();
+    const monthIndex = currentDate.getMonth();
+    const months = [
+        'January', 'February', 'March', 'April', 'May', 'June', 'July',
+        'August', 'September', 'October', 'November', 'December'
+    ];
+    return months[monthIndex];
+}
+
+function getFullDate(){ //example return output = 11:19pm Wednesday 10th Decemebr 2023
+    const currentDate = new Date();
+    const time = getTime();
+    const day = getDay();
+    const month = getMonth();
+    const year = currentDate.getFullYear();
+    const suffix = getDaySuffix(day);
+    const dayOfWeek = getDayOfWeek(currentDate.getDay());
+    return `${dayOfWeek} | ${time} | ${day}${suffix} ${month} ${year}`;
+}
+
+// Helper function to get the suffix for the day (e.g., 1st, 2nd, 3rd)
+function getDaySuffix(day) {
+    if (day >= 11 && day <= 13) {
+      return 'th';
+    }
+    switch (day % 10) {
+      case 1: return 'st';
+      case 2: return 'nd';
+      case 3: return 'rd';
+      default: return 'th';
+    }
+  }
+  
+// Helper function to get the day of the week
+function getDayOfWeek(dayIndex) {
+    const daysOfWeek = [
+      'Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'
+    ];
+    return daysOfWeek[dayIndex];
+}
+  
+module.exports = {
+    getTime,
+    getDay,
+    getMonth,
+    getFullDate
+};
+
+  //console.log(getTime()); // Example output: 11:19pm
+  //console.log(getDay()); // Example output: 10
+  //console.log(getMonth()); // Example output: December
+  //console.log(getFullDate()); // Example output: 11:19pm Wednesday 10th December 2023
