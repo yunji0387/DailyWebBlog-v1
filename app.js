97ad108780a11334a3d8e9c2e461c55618d94fa1
yunji0387 <yunji0387@gmail.com>
2023-06-12 23:59:18 -0500

    update web

--- app.js
+++ app.js
@@ -14,13 +14,27 @@ app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
+const webPages = ["home", "about", "news", "comingsoon", "contact"];
+const stockCompanyList = [
+    {name: "Tesla", symbol: "TSLA", lastUpdate: "-", openPrice: "-", closePrice:"-", highPrice: "-", lowPrice: "-"},
+    {name: "Apple", symbol: "AAPL", lastUpdate: "-", openPrice: "-", closePrice:"-", highPrice: "-", lowPrice: "-"},
+    {name: "Microsoft", symbol: "MSFT", lastUpdate: "-", openPrice: "-", closePrice:"-", highPrice: "-", lowPrice: "-"},
+    {name: "Amazon", symbol: "AMZN", lastUpdate: "-", openPrice: "-", closePrice:"-", highPrice: "-", lowPrice: "-"},
+    {name: "Google", symbol: "GOOGL", lastUpdate: "-", openPrice: "-", closePrice:"-", highPrice: "-", lowPrice: "-"}
+];
 
 mongoose.connect("mongodb://localhost:27017/dailyWebDB", { useNewUrlParser: true });
 
 app.get("/", async function(req, res) {
+    res.redirect("/home");
+});
+
+app.get("/home", async function(req, res){
     const currentTime = date.getFullDate();
-    let currWeather, currStockInfo, currCryptoInfo;
+    let currWeather;
     let currWeatherTemp, currWeatherCity, currWeatherDesc, currWeatherIcon;
+    
+    //get weather api
     try {
         currWeather = await api.getWeatherInfo();
         currWeatherTemp = currWeather.temp;
@@ -32,13 +46,66 @@ app.get("/", async function(req, res) {
             console.error(error);
             currWeatherIcon = "Error fetching weather icon";
         }
-        console.log(currWeatherIcon);
     } catch (error) {
       console.error(error);
       currWeather = "Error fetching weather";
     }
-    res.render("home", { currentTime: currentTime, currentTemp: currWeather.temp, currentWeather: currWeather.description, currentCity: currWeather.name, currentWeatherIcon: currWeatherIcon });
-  });
+
+    let currStock;
+    let currStockSymbol, currStockLastUpdate, currStockOpenPrice, currStockClosePrice, currStockHighPrice, currStockLowPrice;
+
+    //get stock api
+    try {
+        currStock = await api.getStockInfo('TSLA');
+        currStockSymbol = currStock.symbol;
+        currStockLastUpdate = currStock.lastUpdate;
+        currStockOpenPrice = currStock.openPrice;
+        currStockClosePrice = currStock.closePrice;
+        currStockHighPrice = currStock.highPrice;
+        currStockLowPrice = currStock.lowPrice;
+
+    } catch (error) {
+      console.error(error);
+      currStock = "Error fetching weather";
+    }
+
+    res.render("home", { 
+        currentTime: currentTime, 
+        currentTemp: currWeather.temp, 
+        currentWeather: currWeather.description, 
+        currentCity: currWeather.name, 
+        currentWeatherIcon: currWeatherIcon,
+        stockSymbol: currStockSymbol,
+        stockLastUpdate: currStockLastUpdate,
+        stockOpenPrice: currStockOpenPrice,
+        stockClosePrice: currStockClosePrice,
+        stockHighPrice: currStockHighPrice,
+        stockLowPrice:  currStockLowPrice
+    });
+});
+
+app.get("/news", async function(req, res){
+    const currentTime = date.getFullDate();
+    res.render("comingsoon", { currentTime: currentTime });
+});
+
+app.get("/contact", async function(req, res){
+    const currentTime = date.getFullDate();
+    res.render("comingsoon", { currentTime: currentTime });
+});
+
+app.get("/about", async function(req, res){
+    const currentTime = date.getFullDate();
+    res.render("comingsoon", { currentTime: currentTime });
+});
+
+app.get("/:customName", function(req, res){
+    if(webPages.includes(req.params.customName)){
+        res.render(req.params.customName);
+    }else{
+        console.log("Invalid URL path!!!");
+    }
+});
 
 app.post("/", function(req, res){
 
--- logic/api.js
+++ logic/api.js
@@ -29,9 +29,18 @@ require("dotenv").config();
 //     return result;
 // }
 
+const stockCompanyList = [
+  {name: "Tesla", symbol: "TSLA", lastUpdate: "-", openPrice: "-", closePrice:"-", highPrice: "-", lowPrice: "-"},
+  {name: "Apple", symbol: "AAPL", lastUpdate: "-", openPrice: "-", closePrice:"-", highPrice: "-", lowPrice: "-"},
+  {name: "Microsoft", symbol: "MSFT", lastUpdate: "-", openPrice: "-", closePrice:"-", highPrice: "-", lowPrice: "-"},
+  {name: "Amazon", symbol: "AMZN", lastUpdate: "-", openPrice: "-", closePrice:"-", highPrice: "-", lowPrice: "-"},
+  {name: "Google", symbol: "GOOGL", lastUpdate: "-", openPrice: "-", closePrice:"-", highPrice: "-", lowPrice: "-"}
+];
+
 function getWeatherInfo() {
     const weatherAPI_URL = "https://api.openweathermap.org/data/2.5/weather?q=Winnipeg&appid=" + process.env.WEATHER_API_KEY + "&units=metric";
   
+    //using Promise will ensure the result will returned after the "https.get" request and the response is recieved.
     return new Promise((resolve, reject) => {
       https.get(weatherAPI_URL, function(res) {
         let result = {};
@@ -71,9 +80,49 @@ function getWeatherIcon(iconID){
     return result_URL;
 }
 
+function getStockInfo(stockSymbol){
+  const stockAPI_URL = "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol="+ stockSymbol +"&apikey=J09XOSAQI801O3WI";
+  return new Promise((resolve, reject) => {
+    https.get(stockAPI_URL, function(res) {
+      let result = {};
+
+      if (res.statusCode >= 200 && res.statusCode < 300) {
+        let data = "";
+
+        res.on("data", function(chunk) {
+          data += chunk;
+        });
+
+        res.on("end", function() {
+          const stockData = JSON.parse(data);
+          //console.log(stockData);
+          //console.log("-------------------------------");
+          const stockLastUpdate = stockData['Meta Data']['3. Last Refreshed'];
+          result = {
+            symbol: stockData['Meta Data']['2. Symbol'],
+            lastUpdate: stockLastUpdate,
+            openPrice: stockData['Time Series (Daily)'][stockLastUpdate]['1. open'],
+            closePrice: stockData['Time Series (Daily)'][stockLastUpdate]['4. close'],
+            highPrice: stockData['Time Series (Daily)'][stockLastUpdate]['2. high'],
+            lowPrice: stockData['Time Series (Daily)'][stockLastUpdate]['3. low']
+          };
+          console.log(result);
+          resolve(result); // Resolve the promise with the result
+        });
+      } else {
+        result = {
+          status: res.statusCode
+        };
+        resolve(result); // Resolve the promise with the result
+      }
+    }).on("error", function(error) {
+      reject(error); // Reject the promise if an error occurs
+    });
+  });
+}
+
 module.exports = {
     getWeatherInfo,
-    getWeatherIcon
+    getWeatherIcon,
+    getStockInfo
 };
-
-getWeatherInfo();
